1a5a5374f4fa731728e469a1a4a268b48309ce5a
User#None <77580844+issamansur@users.noreply.github.com>
2023-06-03 12:34:42 +0300

    Bug
    Founded by Evgeny
    Fixed by me

---
+++ EditForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Calendar.Database;
+using Calendar.HelpfulElements;
+
+namespace Calendar.HelpfulForms
+{
+    public partial class EditForm : ActionForm
+    {
+        private Event EditEvent { get; init; }
+        
+        public EditForm(Event editEvent) : base(editEvent.DateStart)
+        {
+            EditEvent = editEvent;
+
+            dtPickerStart.Value = editEvent.DateStart;
+            txtBoxTitle.Text = editEvent.Title;
+            dtPickerEnd.Value = editEvent.DateEnd;
+            chkBoxAllDay.Checked = editEvent.AllDay;
+            chkBoxEveryYear.Checked = editEvent.EveryYear;
+            if (editEvent.DateRemind == null)
+                chkBoxRemind.Checked = false;
+
+            Prepare();
+        }
+        
+        private void Prepare()
+        {
+            Text = "Редактирование события";
+            btnAction.Text = "Изменить событие";
+        }
+
+        private protected override bool Action(Event actEvent)
+        {
+            return (Owner as Form1).EditEvent(EditEvent, actEvent);
+        }
+    }
+}
